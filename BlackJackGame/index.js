51c83a39531ce13a3b006fd337c9a7442eb2f4d3
NarsaRao <narsaraoindana@gmail.com>
2025-10-24 17:02:19 +0000

    some changes are made that are breaking game loop

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -1,14 +1,18 @@
-let firstCard=getRandomCard()
-let secondCard=getRandomCard()
-let cards=[firstCard,secondCard]
-let sum=firstCard+secondCard
+let player={
+    name:"Bappi",
+    chips:134
+}
+let cards=[]
+let sum=0
 let hasBlackJack=false
-let isAlive=true
+let isAlive=false
 let message=""
 
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
 let cardsEl=document.getElementById("cards-el")
+let playerEl=document.getElementById("player-el")
+playerEl.textContent=player.name+": $"+player.chips
 
 function renderGame(){
     if(sum<=20){
@@ -32,13 +36,23 @@ function renderGame(){
 
 }
 function startGame(){
+    isAlive=true
+    hasBlackJack=false
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard
+
+
     renderGame()
 }
 function newCard(){
+    if(isAlive===true && hasBlackJack===false){
     let card=getRandomCard()
     sum+=card
     cards.push(card)
     renderGame()
+}
 }
 function getRandomCard(){
     let randomNumber=Math.floor(Math.random()*13)+1
