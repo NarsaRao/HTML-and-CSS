ea90949d73939f2700dc4a47952b3f21c66a51a5
NarsaRao <narsaraoindana@gmail.com>
2025-11-03 12:08:00 +0000

    delete button done and ready for extension usage

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -2,16 +2,40 @@ let myLeads=[]
 const inputEl=document.getElementById("input-el");
 const inputBtn=document.getElementById("input-btn");
 const ulEl=document.getElementById("ul-el");
+const delBtn=document.getElementById("del-btn");
+
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("Lead Tracking"));
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
+    renderLeads()
+}
+delBtn.addEventListener("dblclick",function(){
+    localStorage.clear();
+    myLeads=[];
+    renderLeads();
+
+})
+
+
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
-    renderLeads();
     inputEl.value="";
+    localStorage.setItem("Lead Tracking",JSON.stringify(myLeads));
+    renderLeads();
+
 })
 function renderLeads(){
-        let listItems=`<li>
-        <a href='${inputEl.value}'>${inputEl.value}</a>
-        </li>`;
-        ulEl.innerHTML+=listItems;
+        let listItems = "";
+        for (let i = 0; i < myLeads.length; i++) {
+            listItems += `
+                <li>
+                    <a href="${myLeads[i]}" target="_blank">
+                        ${myLeads[i]}
+                    </a>
+                </li>
+            `;
+        }
+        ulEl.innerHTML = listItems;
 
 }
 
